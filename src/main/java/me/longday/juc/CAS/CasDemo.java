ef655af1db16134d823de582c2bdc054edb3c764
longDay <rosyevening@qq.com>
2022-09-28 18:43:45 +0800

    CAS

---
+++ src/main/java/me/longday/juc/CAS/CasDemo.java
@@ -0,0 +1,13 @@
+package me.longday.juc.CAS;
+
+/**
+ * @author 君
+ * @version 1.0
+ * @desc 原子类
+ * @date 2022/9/28
+ */
+public class CasDemo {
+    public static void main(String[] args) {
+
+    }
+}
